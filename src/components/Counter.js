0d6adc11fe3219250d473befe3fe8cdb122db8b1
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2022-12-03 23:12:06 +0600

    set counter app

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+
+const [count,setCount]=useState(0);
+
+
+const increase=()=>{
+
+  setCount(count+1)
+
+
+}
+
+const decrease=()=>{
+
+  setCount(count-1)
+
+
+}
+
+const reset=()=>{
+
+  setCount(0)
+
+
+}
 
 const Counter = () => {
   return (
     <div className="counter center">
       <h1 className="counter__title">Counter App</h1>
       <div className="card center">
-        <h2 className="card__title">Count : 0</h2>
+        <h2 className="card__title">Count : {count}</h2>
         <div className="card__btns">
-          <button className="btn card__btn">+</button>
-          <button className="btn card__btn">-</button>
-          <button className="btn card__btn">0</button>
+          <button className="btn card__btn" onClick={increase}>+</button>
+          <button className="btn card__btn" onClick={decrease}>-</button>
+          <button className="btn card__btn" onClick={reset}>0</button>
         </div>
       </div>
     </div>
